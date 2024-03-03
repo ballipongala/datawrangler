4f3870214b64a02cac2216e63d79ba9aa35bffc1
Bharath Kumar Ballipongala <bharathb294@gmail.com>
2024-03-03 08:05:21 -0500

    Initial Commit

--- src/Create_ADF.ps1
+++
@@ -1,8 +0,0 @@
-connect-AzAccount
-Get-AzContext -ListAvailable | Where{$_.Name -match 'PAY'} | Set-AzContext 
-
-New-AzResourceGroup   -Name covid19_reporting   -Location "EAST US"
-
-$templateFile="../arm/Create_ADF.json"
-New-AzResourceGroupDeployment   -Name blanktemplate -ResourceGroupName covid19_reporting -TemplateFile $templateFile
- # Remove-AzResourceGroup -Name covid19_reporting -Verbose
